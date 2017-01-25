d60ea29736d877986c6d5d9e3b990c887368efcd
Sashank Allamraju <allamraju.sashank@gmail.com>
2017-01-25 09:02:47 +0530

    code: refactored google maps directions api | fixed: showing alternative paths | to be fixed: infoWindow showing distance/duration

--- js/main.js
+++ js/main.js
@@ -42,3 +42,90 @@ $(window).on('load, resize', function mobileViewUpdate() {
 $('.carousel').carousel({
   interval: 3500
 })
+
+// google map directions
+var map;
+
+// for rendering all routes
+function calculateAndDisplayRoute(origin, destination, directionsService, directionsDisplay, map) {
+  directionsService.route({
+    origin: origin,
+    destination: destination,
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.METRIC,
+    provideRouteAlternatives: true,
+    avoidHighways: false,
+    avoidTolls: false,
+    drivingOptions: { // shows optimistic path under current traffic conditions
+      departureTime: new Date(Date.now()),
+      trafficModel: 'optimistic'
+    }
+  }, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        for (var i = response.routes.length - 1, len = response.routes.length; i > -1; i--) {
+          var strokeColor = (i > 0) ? '#726f68' : 'blue';
+          /* inforwindow yet to be fixed
+          var distance = response.routes[i].legs[0].distance.text;
+          var duration = response.routes[i].legs[0].duration.text;
+          var infowindow = new google.maps.InfoWindow();
+          infowindow.setContent(" distance: "+distance+"<br> duration: "+duration+" ");
+          */
+          new google.maps.DirectionsRenderer({
+            map: map,
+            directions: response,
+            routeIndex: i,
+            polylineOptions: {
+              strokeColor: strokeColor,
+              strokeOpacity: 0.9,
+              strokeWeight: 5}
+          });
+        }
+      } else {
+        window.alert('Unable to find the distance via road. Directions request failed due to - ' + status);
+      }
+  });
+}
+
+
+// for initializing map
+function initMap() {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var map = new google.maps.Map(
+    document.getElementById("map"), {
+      center: new google.maps.LatLng(19.021335, 72.842255),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+  directionsDisplay.setMap(map);
+
+  /* geocoding to be fixed later
+  // fetching coordinates for origin and destination
+  var geocoder = new google.maps.Geocoder();
+  // allocate origin and destination addresses here
+  var originCoordinates = geocodeAddress(geocoder, 'Chhatrapati Shivaji Terminus');
+  //console.log(originCoordinates);
+  var destinationCoordinates = geocodeAddress(geocoder, 'Chhatrapati Shivaji International Airport');
+  //console.log(destinationCoordinates);
+  */
+
+  // render all routes
+  calculateAndDisplayRoute(new google.maps.LatLng(18.9398208,72.8354676), new google.maps.LatLng(19.0895595, 72.8656144), directionsService, directionsDisplay, map);
+}
+
+/* geocoding to be fixed later
+// for geocoding
+function geocodeAddress(geocoder, address) {
+  var addr = address;
+  geocoder.geocode({'address': addr}, function(results, status) {
+    if (status === 'OK') {
+      return results[0].geometry.location;
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+*/
+
+// initialize
+google.maps.event.addDomListener(window, "load", initMap);
